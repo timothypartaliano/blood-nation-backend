a0273829756355588642419a31d098a44f934b5c
Timothy Partaliano <timothypartaliano@gmail.com>
2024-04-03 16:35:41 +0700

    add api get event by id

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -11,6 +11,17 @@ class EventController {
                 // res.status(500).json(err);
             })
     }
+
+    static GetEventByID(req, res) {
+        Event.findByPk(req.params.id)
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch(err => {
+                res.status(500).json({ message: err.message });
+                // res.status(500).json(err);
+            })
+    }
 }
 
 module.exports = EventController;
--- routers/index.js
+++ routers/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const EventController = require('../controllers/eventController');
 
 router.get('/events', EventController.GetAllEvents);
+router.get('/events/:id', EventController.GetEventByID);
 
 module.exports = router;
