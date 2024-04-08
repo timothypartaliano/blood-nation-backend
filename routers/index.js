5674dd79625c3bcd8c26d317861a6a5cf2333a07
Timothy Partaliano <timothypartaliano@gmail.com>
2024-04-08 17:23:32 +0700

    add authentication middleware

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const EventController = require('../controllers/eventController');
 const UserController = require('../controllers/userController');
+const authentication = require('../middlewares/authentication');
 
 router.post('/users/register', UserController.Register);
 router.post('/users/login', UserController.Login);
+
+router.use(authentication);
 
 router.get('/events', EventController.GetAllEvents);
 router.get('/events/:id', EventController.GetEventByID);
