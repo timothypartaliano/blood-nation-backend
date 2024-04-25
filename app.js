e5be9994c3ac754d3ca4f82a5f16207e6f279383
Timothy Partaliano <timothypartaliano@gmail.com>
2024-04-25 14:05:00 +0700

    remove sequelize cache

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const router = require('./routers');
-const { sequelize } = require('./models');
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
-delete require.cache[require.resolve('./models')];
 
 app.use(router);
 
