bc04653733780cc85fef2f369914228d7806f75c
Timothy Partaliano <timothypartaliano@gmail.com>
2024-05-03 10:15:40 +0700

    add jwt token expiration

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,14 +1,15 @@
-const jwt = require("jsonwebtoken")
-const SECRET_KEY = process.env.JWT_SECRET_KEY
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.JWT_SECRET_KEY;
+const TOKEN_EXP = process.env.JWT_TOKEN_EXP;
 
 function generateToken(payload) {
-    const token = jwt.sign(payload, SECRET_KEY)
-    return token
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: TOKEN_EXP });
+    return token;
 }
 
 function verifyToken(token) {
-    const decoded = jwt.verify(token, SECRET_KEY)
-    return decoded
+    const decoded = jwt.verify(token, SECRET_KEY);
+    return decoded;
 }
 
 module.exports = {
