3ecfbe31a3338bc106a1d425f23b1d4cf4d9c1ec
Timothy Partaliano <timothypartaliano@gmail.com>
2024-05-23 00:33:50 +0700

    update event seeder

--- seeders/20240402035231-seeding_event_datas.js
+++ seeders/20240402035231-seeding_event_datas.js
@@ -5,23 +5,127 @@ const { v4: uuidv4 } = require('uuid');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const dummyEvents = Array.from({ length: 10 }).map((_, index) => {
-      const randomDays = Math.floor(Math.random() * 100) + 1;
-      const randomDate = new Date(Date.now() + randomDays * 24 * 60 * 60 * 1000);
-      return {
-        id: uuidv4(),
-        name: `Hospital ${index + 1}`,
-        location: `Location ${index + 1}`,
-        quota: Math.floor(Math.random() * 100) + 1,
-        requirements: `Requirements for Event ${index + 1}`,
-        date: randomDate,
-        image_url: "https://source.unsplash.com/featured/?hospital",
-        created_at: new Date(),
-        updated_at: new Date(),
-      };
-    });
+    const event1 = {
+      id: uuidv4(),
+      name: 'Donor Darah Cengkareng',
+      location: 'RSUD Cengkareng',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 35 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
 
-    await queryInterface.bulkInsert('Events', dummyEvents, {});
+    const event2 = {
+      id: uuidv4(),
+      name: 'Donor Darah Kembangan',
+      location: 'RSUD Kembangan',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 25 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    const event3 = {
+      id: uuidv4(),
+      name: 'Donor Darah Kalideres',
+      location: 'RSUD Kalideres',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 40 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    const event4 = {
+      id: uuidv4(),
+      name: 'Donor Darah Taman Sari',
+      location: 'RSUD Taman Sari',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 30 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    const event5 = {
+      id: uuidv4(),
+      name: 'Donor Darah Cendana',
+      location: 'RSUD Cendana',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 35 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    const event6 = {
+      id: uuidv4(),
+      name: 'Donor Darah Dharmais',
+      location: 'RSUD Dharmais',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 45 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    const event7 = {
+      id: uuidv4(),
+      name: 'Donor Darah Petamburan',
+      location: 'RSUD Petamburan',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 25 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    const event8 = {
+      id: uuidv4(),
+      name: 'Donor Darah Kedoya',
+      location: 'RSUD Kedoya',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 30 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    const event9 = {
+      id: uuidv4(),
+      name: 'Donor Darah Manuela',
+      location: 'RSUD Manuela',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 20 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    const event10 = {
+      id: uuidv4(),
+      name: 'Donor Darah Medika',
+      location: 'RSUD Medika',
+      quota: Math.floor(Math.random() * 100) + 1,
+      requirements: 'Berumur diatas 25 tahun',
+      date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
+      image_url: "https://source.unsplash.com/featured/?hospital",
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    await queryInterface.bulkInsert('Events', [event1, event2, event3, event4, event5, event6, event7, event8, event9, event10], {});
   },
 
   async down (queryInterface, Sequelize) {
