2b650a31e4ae136c7203d7aa927ad321274f2920
Timothy Partaliano <timothypartaliano@gmail.com>
2024-05-28 10:27:28 +0700

    update event image_url

--- seeders/20240402035231-seeding_event_datas.js
+++ seeders/20240402035231-seeding_event_datas.js
@@ -12,7 +12,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 35 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://asset.kompas.com/crops/mOKFrYHlSTM6SEt4aD9PIXZnJE0=/0x5:593x400/750x500/data/photo/2020/03/16/5e6ee88f78835.jpg",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -24,7 +24,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 25 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://awsimages.detik.net.id/community/media/visual/2022/03/19/alamat-rumah-sakit-jakarta-selatan_169.jpeg?w=1200",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -36,7 +36,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 40 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://cdn.antaranews.com/cache/800x533/2022/06/21/FOTO-Advent.png",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -48,7 +48,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 30 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://www.uii.ac.id/wp-content/uploads/2020/03/desain-rumah-sakit-uii.jpg",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -60,7 +60,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 35 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://kedokteran.ubaya.ac.id/wp-content/uploads/sites/13/2021/06/x5jkeutnjmxdyyjcxdqy.jpg",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -72,7 +72,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 45 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://www.rumah123.com/seo-cms/assets/Rumah_Sakit_Advent_5ce2d4599a/Rumah_Sakit_Advent_5ce2d4599a.jpg",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -84,7 +84,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 25 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://news.unair.ac.id/wp-content/uploads/2021/01/PicsArt_01-20-02.17.19-scaled.jpg",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -96,7 +96,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 30 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://img.bandung.go.id/images/news/2023/03/06/header/167807592766-jokowi-resmikan-rumah-sakit-mayapada-kota-bandung-tambah-akses-layanan-kesehatan.jpeg",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -108,7 +108,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 20 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://rs-hamori.co.id/web_new_cms/uploads/1675763721rs-hamori-company.jpg",
       created_at: new Date(),
       updated_at: new Date(),
     };
@@ -120,7 +120,7 @@ module.exports = {
       quota: Math.floor(Math.random() * 100) + 1,
       requirements: 'Berumur diatas 25 tahun',
       date: new Date(Date.now() + (Math.floor(Math.random() * 100) + 1) * 24 * 60 * 60 * 1000),
-      image_url: "https://source.unsplash.com/featured/?hospital",
+      image_url: "https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/06/30/WhatsApp-Image-2023-06-30-at-131725-3915413899.jpeg",
       created_at: new Date(),
       updated_at: new Date(),
     };
