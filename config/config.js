df4eb17f40c7607db44ea22483ff1d758a6af9e2
Timothy Partaliano <timothypartaliano@gmail.com>
2024-06-03 21:31:08 +0700

    change config for deployment

--- config/config.js
+++ config/config.js
@@ -22,11 +22,6 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_DATABASE || 'database_production',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
+    use_env_variable: "DATABASE_URL",
   },
 };
